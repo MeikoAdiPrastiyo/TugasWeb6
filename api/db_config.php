769e37f1144c57acb04d826f5cc5e239e0c9c574
Meiko <185916119+MeikoAdiPrastiyo@users.noreply.github.com>
2024-12-16 20:36:43 +0700

    tugasweb6

---
+++ api/db_config.php
@@ -0,0 +1,11 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123"; //sesuaikan password database kamu
+$db = "webbasic";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+?>
